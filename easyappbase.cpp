480d6d7ff4b17afb541f6799ecdec3799b3b6ad1
James Baker <va7odr@gmail.com>
2024-04-07 11:41:00 -0700

    Kind of ready for work.

--- easyappbase.cpp
+++ easyappbase.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2012-2024 James Baker
+Copyright (c) 2024 James Baker
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -48,6 +48,8 @@ extern const unsigned int HackFont_compressed_size;
 extern const unsigned int HackFont_compressed_data[];
 
 bool EasyAppBase::bQuit = false;
+bool EasyAppBase::bShowEasyAbout = false;
+
 SharedRecursiveMutex EasyAppBase::mtx;
 json::document EasyAppBase::jSaveData;
 SDL_Window* EasyAppBase::window = nullptr;
@@ -58,8 +60,14 @@ EasyAppBase::EasyAppBase(const std::string & sNameIn, const std::string sTitleIn
 
 }
 
-int EasyAppBase::Run()
+int EasyAppBase::Run(const std::string & sAppName, const std::string & sTitle)
 {
+	std::string sWindowTitle;
+	if (sTitle.size()) {
+		sWindowTitle = sTitle;
+	} else {
+		sWindowTitle = sAppName;
+	}
 	AppLogger::CloneToCout(true);
 	if (!std::filesystem::exists(GetAppDataFolder() + APP_NAME)) {
 		std::error_code ec;
@@ -118,8 +126,37 @@ int EasyAppBase::Run()
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+	uint32_t iX = SDL_WINDOWPOS_CENTERED;
+	uint32_t iY = SDL_WINDOWPOS_CENTERED;
+	uint32_t iW = 1280;
+	uint32_t iH = 720;
+
 	SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-	window = SDL_CreateWindow(WINDOW_TITLE " (" __DATE__ ")", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+	{
+		RecursiveSharedLock lock(mtx);
+		if (jSaveData["main_window"].exists("x")) {
+			iX = jSaveData["main_window"]["x"]._uint32();
+		}
+		if (jSaveData["main_window"].exists("y")) {
+			iY = jSaveData["main_window"]["y"]._uint32();
+		}
+		if (jSaveData["main_window"].exists("w")) {
+			iW = jSaveData["main_window"]["w"]._uint32();
+		}
+		if (jSaveData["main_window"].exists("h")) {
+			iH = jSaveData["main_window"]["h"]._uint32();
+		}
+		if (jSaveData["main_window"]["fullscreen"].boolean()) {
+			window_flags = (SDL_WindowFlags)(window_flags | SDL_WINDOW_FULLSCREEN_DESKTOP);
+		} else if (jSaveData["main_window"]["state"] == "maximized") {
+			window_flags = (SDL_WindowFlags)(window_flags | SDL_WINDOW_MAXIMIZED);
+		} else if (jSaveData["main_window"]["state"] == "minimized") {
+			window_flags = (SDL_WindowFlags)(window_flags | SDL_WINDOW_MINIMIZED);
+		}
+	}
+
+	window = SDL_CreateWindow(sWindowTitle.c_str(), iX, iY, iW, iH, window_flags);
 	if (window == nullptr)
 	{
 		printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
@@ -137,6 +174,14 @@ int EasyAppBase::Run()
 	io.IniFilename = (GetAppDataFolder() + APP_NAME + "/imgui.ini").c_str();
 	Log(AppLogger::DEBUG) << "Set ImGui ini file to: " << GetAppDataFolder() << APP_NAME << "/imgui.ini";
 
+	io.Fonts->AddFontFromMemoryCompressedTTF(HackFont_compressed_data, HackFont_compressed_size, 12);
+	io.Fonts->AddFontFromMemoryCompressedTTF(HackFont_compressed_data, HackFont_compressed_size, 18);
+	io.Fonts->AddFontFromMemoryCompressedTTF(HackFont_compressed_data, HackFont_compressed_size, 27);
+	io.Fonts->AddFontFromMemoryCompressedTTF(HackFont_compressed_data, HackFont_compressed_size, 36);
+	io.Fonts->AddFontFromMemoryCompressedTTF(HackFont_compressed_data, HackFont_compressed_size, 45);
+
+	io.FontDefault = io.Fonts->Fonts[*jsonTypedRefTSSh<int>(jSaveData["font_size"], mtx)];
+
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
@@ -145,25 +190,54 @@ int EasyAppBase::Run()
 	//io.ConfigViewportsNoTaskBarIcon = true;
 
 	// Setup Dear ImGui style
-	ImGui::StyleColorsDark();
+	{
+		RecursiveSharedLock lock(mtx);
+		switch(jSaveData["style"]._int()) {
+			case 0:
+			default:
+				ImGui::StyleColorsDark();
+				break;
+
+			case 1:
+				ImGui::StyleColorsLight();
+				break;
+
+			case 3:
+				ImGui::StyleColorsClassic();
+				break;
+		}
+	}
+
 	//ImGui::StyleColorsLight();
 
 	// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
 	ImGuiStyle& style = ImGui::GetStyle();
-	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-	{
-		style.WindowRounding = 0.0f;
-		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-	}
+
+	style.DisplayWindowPadding = {0, 0};
+
+	style.WindowRounding = 8.0;
+	style.ChildRounding = 8.0;
+	style.FrameRounding = 8.0;
+	style.PopupRounding = 8.0;
+	style.ScrollbarRounding = 8.0;
+	style.GrabRounding = 8.0;
+	style.TabRounding = 6.0;
+
+	style.WindowBorderSize = 1.0;
+	style.ChildBorderSize = 1.0;
+	style.PopupBorderSize = 1.0;
+	style.FrameBorderSize = 1.0;
+	style.TabBorderSize = 1.0;
+
+	// if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	// {
+	// 	style.WindowRounding = 0.0f;
+	// 	style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	// }
 
 	// Setup Platform/Renderer backends
 	ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
 	ImGui_ImplOpenGL3_Init(glsl_version);
-
-	io.Fonts->AddFontFromMemoryCompressedTTF(HackFont_compressed_data, HackFont_compressed_size, 12);
-	io.Fonts->AddFontFromMemoryCompressedTTF(HackFont_compressed_data, HackFont_compressed_size, 18);
-	io.Fonts->AddFontFromMemoryCompressedTTF(HackFont_compressed_data, HackFont_compressed_size, 27);
-	io.Fonts->AddFontFromMemoryCompressedTTF(HackFont_compressed_data, HackFont_compressed_size, 36);
 
 	// Our state
 	ImVec4 clear_color = ImVec4(0.0, 0.0, 0.0, 1.0);
@@ -186,9 +260,69 @@ int EasyAppBase::Run()
 
 			if (event.type == SDL_QUIT) {
 				bQuit = true;
-			}
-			if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window)) {
-				bQuit = true;
+			} else if (event.type == SDL_WINDOWEVENT && event.window.windowID == SDL_GetWindowID(window)) {
+				switch (event.window.event) {
+					case SDL_WINDOWEVENT_CLOSE:
+						bQuit = true;
+						break;
+
+					case SDL_WINDOWEVENT_MOVED:
+					{
+						RecursiveExclusiveLock lock(mtx);
+						jSaveData["main_window"]["x"] = event.window.data1;
+						jSaveData["main_window"]["y"] = event.window.data2;
+						break;
+					}
+
+					case SDL_WINDOWEVENT_SIZE_CHANGED:
+					case SDL_WINDOWEVENT_RESIZED:
+					{
+						RecursiveExclusiveLock lock(mtx);
+						jSaveData["main_window"]["w"] = event.window.data1;
+						jSaveData["main_window"]["h"] = event.window.data2;
+						break;
+					}
+
+					case SDL_WINDOWEVENT_MAXIMIZED:
+					{
+						RecursiveExclusiveLock lock(mtx);
+						jSaveData["main_window"]["state"] = "maximized";
+						break;
+					}
+
+					case SDL_WINDOWEVENT_MINIMIZED:
+					{
+						RecursiveExclusiveLock lock(mtx);
+						jSaveData["main_window"]["state"] = "minimized";
+						break;
+					}
+
+					case SDL_WINDOWEVENT_RESTORED:
+					{
+						RecursiveExclusiveLock lock(mtx);
+						SDL_SetWindowFullscreen(window, 0);
+						SDL_SetWindowPosition(window, jSaveData["main_window"]["x"]._uint32(), jSaveData["main_window"]["y"]._uint32());
+						SDL_SetWindowSize(window, jSaveData["main_window"]["w"]._uint32(), jSaveData["main_window"]["h"]._uint32());
+						jSaveData["main_window"]["state"] = "";
+						break;
+					}
+				}
+			} else {
+				switch (event.type) {
+					case SDL_KEYDOWN:
+					{
+
+						break;
+					}
+					case SDL_KEYUP:
+					{
+						switch (event.key.type) {
+
+						}
+
+						break;
+					}
+				}
 			}
 		}
 
@@ -244,23 +378,129 @@ void EasyAppBase::Render()
 	ImGui::SetNextWindowPos(viewport->Pos);
 	ImGui::SetNextWindowSize(viewport->Size);
 	ImGui::SetNextWindowViewport(viewport->ID);
-	ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
 	ImGui::Begin("##Main", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_MenuBar);
-	ImGui::PopStyleVar(2);
+	ImGuiID dockspace_id = 0;
 	if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
-		ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+		dockspace_id = ImGui::GetID("MyDockSpace");
 		ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_None);
 	}
 	Menu();
+	if (bShowEasyAbout) {
+		ImGui::OpenPopup("About EasyAppBase");
+		auto size = viewport->Size;
+		size.x = size.x * 0.5;
+		size.y = size.y * 0.5;
+		auto pos = viewport->Pos;
+		pos.x = pos.x + (viewport->Size.x - size.x) * 0.5;
+		pos.y = pos.y + (viewport->Size.y - size.y) * 0.5;
+
+		ImGui::SetNextWindowPos(pos);
+		ImGui::SetNextWindowSize(size);
+		bShowEasyAbout = false;
+	}
+
+	if (ImGui::BeginPopupModal("About EasyAppBase", nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
+		ImGui::PushFont(io.Fonts->Fonts[4]);
+		ImGui::Text("EasyAppBase v%s (%s)", EASY_APP_VERSION_STRING, EASY_APP_BUILD_DATE);
+		ImGui::PopFont();
+		ImGui::BeginChild("##EasyLicence", {0, 0}, ImGuiChildFlags_FrameStyle);
+		ImGui::TextWrapped(EASY_APP_LICENSE);
+		ImGui::EndChild();
+		ImGui::Text("This application is using:");
+		ImGui::Indent();
+
+		if (ImGui::BeginTable("##EasyAppDepends", 3)) {
+			ImGui::TableSetupColumn("Library", ImGuiTableColumnFlags_NoHide);
+			ImGui::TableSetupColumn("Version", ImGuiTableColumnFlags_NoHide);
+			ImGui::TableSetupColumn("URL", ImGuiTableColumnFlags_NoHide);
+			ImGui::TableHeadersRow();
+
+			ImGui::TableNextRow();
+			ImGui::TableNextColumn();
+			ImGui::Text("SDL2");
+			ImGui::TableNextColumn();
+			ImGui::Text("%s", SDL_GetRevision());
+			ImGui::TableNextColumn();
+			ImGui::Text("https://www.libsdl.org/");
+
+			ImGui::TableNextRow();
+			ImGui::TableNextColumn();
+			ImGui::Text("ImGui");
+			ImGui::TableNextColumn();
+			ImGui::Text("%s", IMGUI_VERSION);
+			ImGui::TableNextColumn();
+			ImGui::Text("https://github.com/ocornut/imgui");
+
+			ImGui::TableNextRow();
+			ImGui::TableNextColumn();
+			ImGui::Text("json_document");
+			ImGui::TableNextColumn();
+			ImGui::Text("%s", JSON_DOCUMENT_VERSION);
+			ImGui::TableNextColumn();
+			ImGui::Text("https://github.com/VA7ODR/json_document");
+
+			ImGui::TableNextRow();
+			ImGui::TableNextColumn();
+			ImGui::Text("SDString");
+			ImGui::TableNextColumn();
+			ImGui::Text("%s", JSON_DOCUMENT_VERSION);
+			ImGui::TableNextColumn();
+			ImGui::Text("https://github.com/VA7ODR/SDString");
+
+			ImGui::TableNextRow();
+			ImGui::TableNextColumn();
+			ImGui::Text("Arbitrary Order Map");
+			ImGui::TableNextColumn();
+			ImGui::Text("%s", JSON_DOCUMENT_VERSION);
+			ImGui::TableNextColumn();
+			ImGui::Text("https://github.com/VA7ODR/ArbitraryOrderMap");
+
+			ImGui::TableNextRow();
+			ImGui::TableNextColumn();
+			ImGui::Text("Boost");
+			ImGui::TableNextColumn();
+			ImGui::Text("%s", BOOST_LIB_VERSION);
+			ImGui::TableNextColumn();
+			ImGui::Text("https://www.boost.org/");
+
+			const GLubyte* version = glGetString(GL_VERSION);
+			ImGui::TableNextRow();
+			ImGui::TableNextColumn();
+			ImGui::Text("OpenGL");
+			ImGui::TableNextColumn();
+			ImGui::Text("%s", version);
+			ImGui::TableNextColumn();
+			ImGui::Text("https://www.opengl.org/");
+
+			ImGui::TableNextRow();
+			ImGui::TableNextColumn();
+			ImGui::Text("OpenSSL");
+			ImGui::TableNextColumn();
+			ImGui::Text("%s", OPENSSL_FULL_VERSION_STR);
+			ImGui::TableNextColumn();
+			ImGui::Text("https://www.openssl.org/");
+
+
+			ImGui::EndTable();
+		}
+
+
+
+		ImGui::Unindent();
+		if (ImGui::Button("Close")) {
+			ImGui::CloseCurrentPopup();
+		}
+		ImGui::EndPopup();
+	}
+
 	auto & registry = *EasyAppBase::Registry();
 	for (auto & window : registry) {
 		bool bShow = jSaveData["show"][window.first].boolean();
 		if (window.second->BuildsOwnWindow()) {
-			window.second->Render(&bShow);
+			window.second->Render(&bShow, dockspace_id);
 		} else {
 			if (ImGui::Begin(window.second->Title().c_str(), &bShow)) {
-				window.second->Render(&bShow);
+				window.second->Render(&bShow, dockspace_id);
 				ImGui::End();
 			}
 		}
@@ -306,6 +546,69 @@ void EasyAppBase::Menu()
 		ImGui::EndMenu();
 	}
 
+	if (ImGui::BeginMenu("View")) {
+		RecursiveExclusiveLock lock(mtx);
+
+		if (ImGui::MenuItem("Fullscreen", "F11", jSaveData["main_window"]["fullscreen"].boolean())) {
+			jSaveData["main_window"]["fullscreen"] = !jSaveData["main_window"]["fullscreen"].boolean();
+			if (jSaveData["main_window"]["fullscreen"].boolean()) {
+				SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+			} else {
+				SDL_SetWindowFullscreen(window, 0);
+				SDL_SetWindowPosition(window, jSaveData["main_window"]["x"]._uint32(), jSaveData["main_window"]["y"]._uint32());
+				SDL_SetWindowSize(window, jSaveData["main_window"]["w"]._uint32(), jSaveData["main_window"]["h"]._uint32());
+			}
+		}
+
+		ImGui::Separator();
+
+		if (ImGui::MenuItem("Scale: Extra Small", nullptr, jSaveData["font_size"] == 0)) {
+			ImGuiIO& io = ImGui::GetIO(); (void)io;
+			io.FontDefault = io.Fonts->Fonts[0];
+			jSaveData["font_size"] = 0;
+		}
+		if (ImGui::MenuItem("Scale: Small", nullptr, jSaveData["font_size"] == 1)) {
+			ImGuiIO& io = ImGui::GetIO(); (void)io;
+			io.FontDefault = io.Fonts->Fonts[1];
+			jSaveData["font_size"] = 1;
+		}
+		if (ImGui::MenuItem("Scale: Default", nullptr, jSaveData["font_size"] == 2)) {
+			ImGuiIO& io = ImGui::GetIO(); (void)io;
+			io.FontDefault = io.Fonts->Fonts[2];
+			jSaveData["font_size"] = 2;
+		}
+		if (ImGui::MenuItem("Scale: Large", nullptr, jSaveData["font_size"] == 3)) {
+			ImGuiIO& io = ImGui::GetIO(); (void)io;
+			io.FontDefault = io.Fonts->Fonts[3];
+			jSaveData["font_size"] = 3;
+		}
+		if (ImGui::MenuItem("Scale: Extra Large", nullptr, jSaveData["font_size"] == 4)) {
+			ImGuiIO& io = ImGui::GetIO(); (void)io;
+			io.FontDefault = io.Fonts->Fonts[4];
+			jSaveData["font_size"] = 4;
+		}
+
+		ImGui::Separator();
+
+		if (ImGui::MenuItem("Style: Dark", nullptr, jSaveData["style"] == 0)) {
+			ImGuiIO& io = ImGui::GetIO(); (void)io;
+			ImGui::StyleColorsDark();
+			jSaveData["style"] = 0;
+		}
+		if (ImGui::MenuItem("Style: Light", nullptr, jSaveData["style"] == 1)) {
+			ImGuiIO& io = ImGui::GetIO(); (void)io;
+			ImGui::StyleColorsLight();
+			jSaveData["style"] = 1;
+		}
+		if (ImGui::MenuItem("Style: Dark", nullptr, jSaveData["style"] == 2)) {
+			ImGuiIO& io = ImGui::GetIO(); (void)io;
+			ImGui::StyleColorsClassic();
+			jSaveData["style"] = 2;
+		}
+
+		ImGui::End();
+	}
+
 	if (ImGui::BeginMenu("Windows"))	{
 		auto & registry = *EasyAppBase::Registry();
 		for (auto & window : registry) {
@@ -314,6 +617,16 @@ void EasyAppBase::Menu()
 				bShow = !bShow;
 			}
 			jSaveData["show"][window.first] = bShow;
+		}
+		ImGui::EndMenu();
+	}
+
+	if (ImGui::BeginMenu("Help"))	{
+		auto & registry = *EasyAppBase::Registry();
+		for (auto & window : registry) {
+			if (ImGui::MenuItem("About EasyAppBase...", "")) {
+				bShowEasyAbout = true;
+			}
 		}
 		ImGui::EndMenu();
 	}
@@ -332,3 +645,8 @@ refTSEx<json::value> EasyAppBase::ExclusiveSettings(const std::string & sName)
 {
 	return {jSaveData["sub"][sName], mtx};
 }
+
+void EasyAppBase::Init() {
+	// GenerateWindow<DemoWindow>();
+}
+
--- easyappbase.hpp
+++ easyappbase.hpp
@@ -1,31 +1,29 @@
-/*
-Copyright (c) 2012-2024 James Baker
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-The official repository for this library is at https://github.com/VA7ODR/EasyAppBase
-
-*/
+#define EASY_APP_LICENSE \
+	"Copyright (c) 2024 James Baker" \
+	"\n" \
+	"Permission is hereby granted, free of charge, to any person obtaining a copy " \
+	"of this software and associated documentation files (the \"Software\"), to dea l" \
+	"in the Software without restriction, including without limitation the rights " \
+	"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell " \
+	"copies of the Software, and to permit persons to whom the Software is " \
+	"furnished to do so, subject to the following conditions:\n" \
+	"\n" \
+	"The above copyright notice and this permission notice shall be included in " \
+	"all copies or substantial portions of the Software.\n" \
+	"\n" \
+	"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR " \
+	"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, " \
+	"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE " \
+	"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER " \
+	"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, " \
+	"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN " \
+	"THE SOFTWARE.\n" \
+	"\n" \
+	"The official repository for this library is at https://github.com/VA7ODR/EasyAppBase"
 
 #pragma once
+
 #include "app_logger.hpp"
-//#include "config.hpp"
 #include "utils.hpp"
 #include "data.hpp"
 #include "eventhandler.hpp"
@@ -34,22 +32,29 @@ The official repository for this library is at https://github.com/VA7ODR/EasyApp
 #include "shared_recursive_mutex.hpp"
 #include "imgui.h"
 #include <SDL.h>
-#include "config.hpp"
+
+#define EASY_APP_VERSION_MAJOR 1
+#define EASY_APP_VERSION_MINOR 0
+#define EASY_APP_VERSION_PATCH 0
+
+#define EASY_APP_VERSION_STRING "1.0.0"
+#define EASY_APP_BUILD_DATE __DATE__
 
 class EasyAppBase
 {
 	public:
 		EasyAppBase(const std::string & sNameIn, const std::string sTitleIn);
+		virtual ~EasyAppBase() {}
 
 		virtual void Start() {};
-		virtual void Render(bool * bShow) = 0;
+		virtual void Render(bool * bShow, ImGuiID dockspace_id) = 0;
 		virtual void Stop() {};
 		virtual bool BuildsOwnWindow() { return false; }
 
 		const std::string & Name() const { return sName; }
 		const std::string & Title() const { return sTitle; }
 
-		static int Run();
+		static int Run(const std::string & sAppName, const std::string & sTitle = "");
 		static void Render();
 
 		refTSEx<json::value> ExclusiveSettings();
@@ -83,6 +88,7 @@ class EasyAppBase
 		static refTSSh<json::value> SharedSettings(const std::string & sName);
 
 		static bool bQuit;
+		static bool bShowEasyAbout;
 
 		static SharedRecursiveMutex mtx;
 		static json::document jSaveData;
@@ -92,13 +98,9 @@ class EasyAppBase
 
 		std::string sName;
 		std::string sTitle;
+
+
 };
-
-#define INIT_HEADER void EasyAppBase::Init() {
-#define ADD_WINDOW(X) GenerateWindow<X>()
-#define INIT_FOOTER }
-
-
 
 class DemoWindow : public EasyAppBase
 {
@@ -106,11 +108,10 @@ class DemoWindow : public EasyAppBase
 		DemoWindow() : EasyAppBase("demo_window", "ImGui Demo Window") {}
 		bool BuildsOwnWindow() override { return true; }
 
-		void Render(bool * bShow) override
+		void Render(bool * bShow, ImGuiID dockspace_id) override
 		{
 			if (*bShow) {
 				ImGui::ShowDemoWindow(bShow);
 			}
 		}
 };
-
